3ed57bc6e1a58a7f02426140df14ad3e52827c2c
Rafael <k.rafaelalves@gmail.com>
2022-05-10 00:23:32 -0300

    starting deploy

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -1,18 +1,18 @@
-import { Camera, ClockCounterClockwise, Trash } from "phosphor-react";
-import html2canvas from 'html2canvas';
-import { useState } from "react";
-import { Loading } from "../Loading";
+import { Camera, Trash } from "phosphor-react"
+import html2canvas from 'html2canvas'
+import { useState } from "react"
+import { Loading } from "../Loading"
 
 interface ScreenshotButtonProps {
-    screenshot: string | null
     onScreenshotTook: (screenshot: string | null) => void
+    screenshot: string | null
+    
 }
 
 export function ScreenshotButton({ screenshot, onScreenshotTook }: ScreenshotButtonProps) {
     const [isTakingScreenshot, setIsTakingScreenshot] = useState(false)
 
-    async function handleTakeScreenshot() {
-  
+    async function handleTakeScreenshot() {  
         setIsTakingScreenshot(true)
 
         const canvas = await html2canvas(document.querySelector('html')!)
@@ -27,12 +27,12 @@ export function ScreenshotButton({ screenshot, onScreenshotTook }: ScreenshotBut
             <button
                 type="button"
                 className="p-1 w-10 h-10 rounded border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
-                onClick={() => onScreenshotTook(null)}
                 style={{
                     backgroundImage: `url(${screenshot})`,
                     backgroundPosition: 'right bottom',
                     backgroundSize: 180,
                 }}
+                onClick={() => onScreenshotTook(null)}
             >
                 <Trash weight="fill" />
             </button>
@@ -41,12 +41,12 @@ export function ScreenshotButton({ screenshot, onScreenshotTook }: ScreenshotBut
 
     return(
         <button
-        type="button"
-        onClick={handleTakeScreenshot}
-        className="p-2 bg-zinc-800 rounded border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
-    >
-        { isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6"/> }
+            type="button"
+            onClick={handleTakeScreenshot}
+            className="p-2 bg-zinc-800 rounded border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+        >
+            { isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6"/> }
         
-    </button>   
+        </button>   
     )
 }
